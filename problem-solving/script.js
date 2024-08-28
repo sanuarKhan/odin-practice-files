62dcd226b4cc92db000139b7235c4364843efb39
sanuarKhan <[sanuarkhan220@gmail.com]>
2024-08-29 00:56:49 +0600

    problem solving section done

--- problem-solving/script.js
+++ problem-solving/script.js
@@ -1,1 +1,13 @@
 let answer = parseInt(prompt("Please enter the number"));
+
+for (let i = 1; i <= answer; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    document.write("FizzBuzz <br>");
+  } else if (i % 3 === 0) {
+    document.write("Fizz <br>");
+  } else if (i % 5 === 0) {
+    document.write("Buzz <br>");
+  } else {
+    document.write(i, "<br>");
+  }
+}
